60fd1d3b64ee01ec71f8ff39d7a29b8d32dbbcc8
Takeyuki UEDA <ueda@latelierdueda.com>
2019-04-25 21:43:57 +0900

    add link to pokemon

--- src/views/Types.vue
+++ src/views/Types.vue
@@ -39,7 +39,13 @@
       </v-card>
 
     </v-card>
-    <v-btn small v-for="(pokemon, index) in poke.filter(member => member.type.includes($route.params.id))" :key="index">{{pokemon.name.japanese}}</v-btn>
+    <router-link 
+      v-for="(pokemon, index) in poke.filter(member => member.type.includes($route.params.id))"
+      v-bind:to="path_to_pokemon_with_id(pokemon.id)"
+      :key="index"
+    >
+      <v-btn small>{{pokemon.name[l_lang]}}</v-btn>
+    </router-link>
   </div>
 </template>
 
@@ -60,12 +66,18 @@ module.exports = {
   data: function () {
     return {
       s_lang: "ja",
+      l_lang: "japanese",
     }
   },
   methods: {
     path_to_types_with_id: function(id){
       return "/types/" + id;
-    }
+    },
+    path_to_pokemon_with_id: function(id){
+//      return "/pokemon/" + this.props.poke.filter(member => member.name[this.l_lang] == nm)[0].id;
+      return "/pokemon/" + id;
+    },
+
   },
 
 }
