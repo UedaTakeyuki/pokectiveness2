728a442ad1ca57f5e2c1ec0af4e44b65d406fa1f
Takeyuki UEDA <ueda@latelierdueda.com>
2019-04-28 11:06:38 +0900

    back navigation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,11 +3,10 @@
     <v-toolbar app>
       <v-toolbar-side-icon></v-toolbar-side-icon>
       <v-toolbar-title class="grey--text">
-        <span class="font-weight-light">Pokectiveness</span>
-        <span>2</span>
+        <span class="font-weight-light" style="font-family: 'Parisienne', cursive; text-shadow: 4px 4px 4px #aaa; font-size: 110%;">Pokéctiveness</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn flat class="grey--text">
+      <v-btn flat class="grey--text" v-if="$route.name != 'home'" @click="$router.back()">
         <span>戻る</span>
       </v-btn>
     </v-toolbar>
