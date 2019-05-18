9c6040e4841d6e1f9fefc0d498e04dea46860216
Takeyuki UEDA <ueda@latelierdueda.com>
2019-05-18 11:01:20 +0900

    i18n

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,25 @@ module.exports = {
   title: 'Pokéctiveness2',
   description: 'Pokémon move effectiveness chart.',
   dest: 'dist/docs',
-  base: '/docs/'
+//  dest: 'public/docs',
+  base: '/docs/',
+
+  themeConfig: {
+    sidebar: 'auto'
+  },
+
+  locales: {
+    // The key is the path for the locale to be nested under.
+    // As a special case, the default locale can use '/' as its path.
+    '/': {
+      lang: 'english', // this will be set as the lang attribute on <html>
+      title: 'Pokectiveness2',
+      description: 'Pokémon move effectiveness chart.'
+    },
+    '/ja/': {
+      lang: '日本語',
+      title: 'Pokectiveness2',
+      description: 'ポケモンわざ相性図鑑'
+    }
+  }
 }
