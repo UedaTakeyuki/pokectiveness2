2ce628910fe2d776e894fa66aaaf559e41ab4fea
Takeyuki UEDA <ueda@latelierdueda.com>
2019-05-22 19:16:11 +0900

    add back button

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,9 @@ module.exports = {
   title: 'Pokéctiveness2',
   description: 'Pokémon move effectiveness chart.',
   dest: 'dist/docs',
-//  dest: 'public/docs',
   base: '/docs/',
+//  dest: 'public/docs',
+//  base: '/public/docs/',
 
   themeConfig: {
     sidebar: 'auto'
@@ -13,14 +14,22 @@ module.exports = {
     // The key is the path for the locale to be nested under.
     // As a special case, the default locale can use '/' as its path.
     '/': {
-      lang: 'english', // this will be set as the lang attribute on <html>
+      lang: 'english', // this will be set as the lang attribute 
+      on <html>
       title: 'Pokectiveness2',
       description: 'Pokémon move effectiveness chart.'
     },
     '/ja/': {
       lang: '日本語',
       title: 'Pokectiveness2',
-      description: 'ポケモンわざ相性図鑑'
+      description: 'ポケモンわざ相性図鑑',
+      themeConfig: {
+        sidebar: 'auto',
+
+        nav: [
+          { text: '戻る', link: '/' },
+        ]
+      },
     }
   }
 }
