5e5d3282fccbe447da38c616a4f3f3a355cdf485
Takeyuki UEDA <ueda@latelierdueda.com>
2020-07-20 11:07:06 +0900

    basic feature of last feed

--- src/views/Memo.vue
+++ src/views/Memo.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="memo">
     <v-card>
-      <p>実をあげてから{{elapsOfFeed}}分たった</p>
-      <v-btn @click="setElapsOfFeed">今、実をあげた</v-btn>
+      <p>実をあげてから{{elapseOfFeed}}秒たった</p>
+      <v-btn @click="setElapseOfFeed">今、実をあげた</v-btn>
     </v-card>
     <v-card>
-      <p>実をあげてから{{elapsOfFeed}}分たった</p>
+      <p>実をあげてから{{elapseOfFeed}}分たった</p>
       <v-select
         v-model="remainingMinutes"
         :items="items"
@@ -24,16 +24,36 @@ export default {
   components: {},
   data () {
     return {
-      elapsOfFeed: 0,
+      elapseOfFeed: 0,
       items: minutesRange,
       remainingMinutes: 0,
       //
     }
   },
   methods: {
-    setElapsOfFeed: () => {
-      alert("あほー")
+    setElapseOfFeed: function(){
+      let now = new Date()
+      localStorage.elapseOfFeed = now.getTime().toFixed()
+      this.oneSecCrock()
     },
+    oneSecCrock: function(){
+      let lastFeedTime = parseInt(localStorage.elapseOfFeed)
+      let now = new Date()
+      let elapsed = Math.round((now.getTime() - lastFeedTime)/1000)
+      if (elapsed >= 3600 * 24){
+        // Not interested in yesterday
+        this.elapseOfFeed = 0
+      } else {
+        this.elapseOfFeed = elapsed
+        setTimeout(this.oneSecCrock, 1000)
+      }
+    }
+  },
+  mounted: function(){
+    if (localStorage.elapseOfFeed) {
+      this.oneSecCrock()
+    }
   }
+  
 }
 </script>
