fdc1dd0ac7e67140bbccdbcc3750b4cad7b34bb2
Takeyuki UEDA <ueda@latelierdueda.com>
2023-03-14 21:04:47 +0900

    remove icon resources

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,8 +3,6 @@ const assets = [
   '/',
   '/index.html',
   '/favicon.png',
-  'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900',
-  'https://fonts.googleapis.com/css?family=Material+Icons',
   'https://fonts.googleapis.com/css?family=Parisienne',
   'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
   '/images/icons/*.png',
